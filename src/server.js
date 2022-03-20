4fb35fb91f88a8dc1ddde265f07199dc63bc287f
L2HYUNN <dhl9810@naver.com>
2022-03-21 01:44:07 +0900

    #0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
 const handleListening = () => {
   console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
 };
