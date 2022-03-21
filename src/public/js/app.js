b378b9125658844466ffab1debc05be194e72a7a
L2HYUNN <dhl9810@naver.com>
2022-03-21 21:55:52 +0900

    #1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,24 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 
 const URL = `ws://${window.location.host}`;
 
 const socket = new WebSocket(URL);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("✅ Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
   console.log("New message", message.data);
 });
 
@@ -20,11 +29,19 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
-messageForm, addEventListener("submit", handleSubmit);
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
 // setTimeout(() => {
 //   socket.send("hello from the browser");
