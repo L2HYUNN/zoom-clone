a24fc7e58a1f611111d4de35288efd4da26bf998
L2HYUNN <dhl9810@naver.com>
2022-03-21 11:37:33 +0900

    #1.2 WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 
 const PORT = 3000;
 
@@ -8,11 +10,17 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListening = () => {
   console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
 };
 
-app.listen(PORT, handleListening);
+// app.listen(PORT, handleListening);
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+server.listen(PORT, handleListening);
