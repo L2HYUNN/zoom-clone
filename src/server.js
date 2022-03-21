29b9bf3b2de484d2d24e524b20c0e5cc13b19588
L2HYUNN <dhl9810@naver.com>
2022-03-21 22:33:30 +0900

    #1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -27,12 +27,23 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anonymous";
   console.log("✅ Connected to Browser");
   socket.on("close", () => {
     console.log("❌ Disconnected from Browser");
   });
-  socket.on("message", (message) => {
-    sockets.forEach((socket) => socket.send(message.toString("utf8")));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((socket) =>
+          socket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
