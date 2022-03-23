f1420e6aa1bff16bbf739622be3e3cccbee11aa2
L2HYUNN <dhl9810@naver.com>
2022-03-24 00:39:17 +0900

    #2.4 Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,16 +1,26 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
 
-function backendDone(msg) {
-  console.log(`backend says: ${msg}`);
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName = null;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Romm ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, backendDone);
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
