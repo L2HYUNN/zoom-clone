789649684b12a99bcbc37b8821b0aa4677f03080
L2HYUNN <dhl9810@naver.com>
2022-03-24 23:55:07 +0900

    #2.11 Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import http from "http";
 // import WebSocket, { WebSocketServer } from "ws";
 
@@ -21,7 +22,15 @@ const handleListening = () => {
 // app.listen(PORT, handleListening);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
