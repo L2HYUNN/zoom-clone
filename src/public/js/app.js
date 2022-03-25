d90f88ab32f0a70888ac12b6172f41b44f427370
L2HYUNN <dhl9810@naver.com>
2022-03-25 22:10:16 +0900

    #3.8 Senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -83,6 +83,13 @@ function handleCameraBtnClick(event) {
 
 async function handleCameraChange(event) {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteBtnClick);
