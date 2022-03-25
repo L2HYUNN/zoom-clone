12aa581f9624a62cb34c6f0947e2c4132b145f52
L2HYUNN <dhl9810@naver.com>
2022-03-25 19:13:47 +0900

    #3.6 Answers

--- src/server.js
+++ src/server.js
@@ -25,11 +25,13 @@ const wsServer = SocketIO(httpServer);
 wsServer.on("connection", (socket) => {
   socket.on("join_room", (roomName, done) => {
     socket.join(roomName);
-    done();
     socket.to(roomName).emit("welcome");
   });
   socket.on("offer", (offer, roomName) => {
     socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
   });
 });
 
