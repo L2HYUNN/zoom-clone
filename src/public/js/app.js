c7e164a54a6ff557f51955b751405777c05d5e5c
L2HYUNN <dhl9810@naver.com>
2022-03-25 21:17:48 +0900

    #3.7 IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -123,21 +123,42 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+  console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
   myPeerConnection.setLocalDescription(answer);
   socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
 socket.on("answer", (answer) => {
+  console.log("received the answer");
   myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  console.log("received Candidate");
+  myPeerConnection.addIceCandidate(ice);
 });
 
 //RTC Code
 
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("track", handleAddStream);
   myStream.getTracks().forEach((track) => {
     myPeerConnection.addTrack(track, myStream);
   });
+}
+
+function handleIce(data) {
+  console.log("sent Candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  console.log("Stream Date", data);
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.streams[0];
 }
